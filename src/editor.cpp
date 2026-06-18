1e2d9971382e08f2e038f55b584235d6b8903e6b
Lt-Henry <quiqueiii@gmail.com>
2026-06-18 23:47:43 +0200

    almost completed lua highlighter. There is a bug with string regex, however

--- src/editor.cpp
+++ src/editor.cpp
@@ -3,12 +3,68 @@
 using namespace mg32;
 using namespace std;
 
+LuaHighlighter::LuaHighlighter(QTextDocument* parent) : QSyntaxHighlighter(parent)
+{
+    m_keywords << "function" << "end"<<"if"<<"else"<<"elseif"<<"then"<<"for"<<"while"<<"do"<<"local"<<"or"<<"and"<<"not";
+    m_ops<<"="<<"\\+"<<"\\-"<<"\\+"<<"\\/"<<"=="<<"<"<<">";
+
+    m_keywordFormat.setForeground(QColor("#5b6ee1"));
+    m_keywordFormat.setFontWeight(QFont::Bold);
+
+    m_opFormat.setForeground(QColor("#5b6ee1"));
+    m_opFormat.setFontWeight(QFont::Bold);
+
+    m_spaceFormat.setForeground(QColor("#9badb7"));
+    m_stringFormat.setForeground(QColor("#37946e"));
+    m_numberFormat.setForeground(QColor("#d77bba"));
+
+    m_constFormat.setForeground(QColor("#d95763"));
+    m_constFormat.setFontWeight(QFont::Bold);
+
+    m_commentFormat.setForeground(QColor("#9badb7"));
+
+    m_rules << qMakePair(QRegularExpression("\\b(" + m_keywords.join("|") + ")\\b"), m_keywordFormat)
+                << qMakePair(QRegularExpression("(" + m_ops.join("|") + ")"), m_opFormat)
+                << qMakePair(QRegularExpression("\".*\""), m_stringFormat)
+                << qMakePair(QRegularExpression("\'.*\'"), m_stringFormat)
+                << qMakePair(QRegularExpression("\\b([0-9]+)\\b"), m_numberFormat)
+                << qMakePair(QRegularExpression("\\b(0x[0-9a-fA-F]+)\\b"), m_numberFormat)
+                << qMakePair(QRegularExpression("\\b(true|false)\\b"), m_numberFormat)
+                << qMakePair(QRegularExpression("\\b([A-Z_][A-Z0-9_]*)\\b"), m_constFormat)
+                << qMakePair(QRegularExpression("\\s"), m_spaceFormat)
+                << qMakePair(QRegularExpression("(--)[^\n]*"), m_commentFormat);
+}
+
+LuaHighlighter::~LuaHighlighter()
+{
+
+}
+
+void LuaHighlighter::highlightBlock(const QString &text)
+{
+    for (const auto &rule : m_rules) {
+        QRegularExpression expression(rule.first);
+        QRegularExpressionMatchIterator iterator = expression.globalMatch(text);
+        while (iterator.hasNext()) {
+            QRegularExpressionMatch match = iterator.next();
+            setFormat(match.capturedStart(), match.capturedLength(), rule.second);
+        }
+    }
+}
+
 Editor::Editor(QWidget *parent) : QPlainTextEdit(parent)
 {
     setFont(QFont("Hack", 12));
+
+    QTextCharFormat defaultFormat;
+    defaultFormat.setForeground(QColor("#696a6a"));
+    setCurrentCharFormat(defaultFormat);
+
     QTextOption option =  document()->defaultTextOption();
     option.setFlags(option.flags() | QTextOption::ShowTabsAndSpaces);
     document()->setDefaultTextOption(option);
+
+    m_highlighter = new LuaHighlighter(document());
 
 }
 
--- src/editor.hpp
+++ src/editor.hpp
@@ -2,23 +2,59 @@
 #ifndef MG32_EDITOR
 #define MG32_EDITOR
 
+#include <QStringList>
 #include <QPlainTextEdit>
 #include <QSyntaxHighlighter>
 #include <QTextCharFormat>
 #include <QRegularExpression>
 #include <QFont>
 #include <QColor>
+#include <QList>
+#include <QPair>
 
 #include <iostream>
 
 namespace mg32
 {
+
+    class LuaHighlighter : public QSyntaxHighlighter
+    {
+        public:
+
+        LuaHighlighter(QTextDocument* parent);
+        virtual ~LuaHighlighter();
+
+        protected:
+
+        void highlightBlock(const QString& text) override;
+
+        private:
+
+        QStringList m_keywords;
+        QStringList m_ops;
+
+        QList<QPair<QRegularExpression, QTextCharFormat>> m_rules;
+
+        QTextCharFormat m_keywordFormat;
+        QTextCharFormat m_opFormat;
+        QTextCharFormat m_stringFormat;
+        QTextCharFormat m_numberFormat;
+        QTextCharFormat m_constFormat;
+        QTextCharFormat m_commentFormat;
+        QTextCharFormat m_spaceFormat;
+
+    };
+
     class Editor: public QPlainTextEdit
     {
         public:
 
         Editor(QWidget *parent = nullptr);
         virtual ~Editor();
+
+        private:
+
+        LuaHighlighter* m_highlighter;
     };
 }
 
